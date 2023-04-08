e0395958f0d0367b65bd22e4918925c2b15aadff
iso <ismailkurtlojistik@gmail.com>
2023-04-08 13:54:45 +0300

    id.js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -24,11 +24,24 @@ const PostDetail = ({ post }) => {
     );
 };
 
-export const getServerSideProps = async (context) => {
-    const res = await fetch(
-        `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-    );
+export const getStaticPaths = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+    const posts = await res.json();
+
+    const paths = posts.map((post) => {
+        return {
+            params: { id: post.id },
+        }
+    })
+    return {
+        paths,
+        fallback: true
+    };
+}
+export const getStaticProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
     const post = await res.json();
+
     return {
         props: {
             post,
