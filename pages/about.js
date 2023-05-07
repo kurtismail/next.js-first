12f94f76e2c74aff7d1094831e7f1c28a3ac4382
iso <ismailkurtlojistik@gmail.com>
2023-05-07 14:37:18 +0300

    css

--- pages/about.js
+++ pages/about.js
@@ -7,17 +7,21 @@ export default function About() {
                 <title>About Page</title>
                 <link rel="icon" href="/react.svg" />
             </Head>
-            <h1 className="redColor">about</h1>
+            <h1 className="redColor">The React Framework
+                for the Web</h1>
+            <p>Used by some of the world's largest companies, Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.</p>
             <style jsx> {`
                 .greenColor {
                     color:green;
                     background-color:black;
+                    text-align:center;
                 }
                 .redColor {
-                    color: green;
-                    font-size: 100px
+                    color: gray;
+                    font-size: 30px;
+                    text-align: center;
                 }
-            
+
             `} </style>
         </>
     );
