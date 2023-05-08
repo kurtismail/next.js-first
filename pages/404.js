44e41f19b9a678febe7794dd48d8971a863e4606
iso <ismailkurtlojistik@gmail.com>
2023-05-08 18:52:01 +0300

    not found

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@
 
 export default function Notfound() {
     return <div>
-        <h1>404</h1>
+        <h1>404 Not found</h1>
         <p>Sorry, the page you are looking for could not be found.</p>
     </div>
 }
