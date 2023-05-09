ec08e704593722d7ec924c9f87b50badb463daed
iso <ismailkurtlojistik@gmail.com>
2023-05-09 19:42:00 +0300

    cssss

--- pages/about.js
+++ pages/about.js
@@ -24,7 +24,7 @@ export default function About() {
                     margin-top:2%;
                 }
                 p {
-                    margin-bottom:35%;
+                    margin-bottom:34%;
                 }
 
             `} </style>
